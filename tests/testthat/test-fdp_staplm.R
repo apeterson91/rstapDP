c9cdb171fe4d117cdd2e55224c3c95bad4ec58db
apeterson91 <adampeterso@gmail.com>
2020-07-16 14:25:52 -0400

    Add first few preliminary tests

--- tests/testthat/test-fdp_staplm.R
+++ tests/testthat/test-fdp_staplm.R
@@ -1,3 +1,10 @@
-test_that("formula checks", {
-  expect_error(fdp_staplm(BMI ~ sex + sap(FFR)))
+test_that("parameter checks", {
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,iter_max = 0,burn_in = 5))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,iter_max = 3,burn_in = 5))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,alpha_a = -3))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,alpha_b = -3))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,tau_a = -3))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,tau_b = -3))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,sigma_a = -3))
+  expect_error(fdp_staplm(BMI ~ sex + sap(FFR),benvo = FFR_benvo,sigma_b = -3))
 })
