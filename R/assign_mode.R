ea0c016577f701169e432fa60cad0c862a845bc1
apeterson91 <adampeterso@gmail.com>
2020-10-26 13:15:15 -0400

    naming error

--- R/assign_mode.R
+++ R/assign_mode.R
@@ -32,19 +32,19 @@ bendr::assign_mode
 green_loss.stapDP <- function(object, truth = NULL, tau = 0.5, a = 1, b = 1){
 
 	if(is.null(truth)){
-		loss <- green_loss_unknown(object$cluster_assignment,object$pmat,tau)
+		loss <- green_loss_unknown(object$cmat,object$pmat,tau)
 		ix <- which.max(loss)
 	}
 	else{
-		stopifnot(dim(truth)[1] == ncol(object$cluster_assignment))
-		stopifnot(dim(truth)[2] == ncol(object$cluster_assignment))
-		loss <- green_loss_known(object$cluster_assignment,object$pmat,truth,a,b)
+		stopifnot(dim(truth)[1] == ncol(object$cmat))
+		stopifnot(dim(truth)[2] == ncol(object$cmat))
+		loss <- green_loss_known(object$cmat,object$pmat,truth,a,b)
 		ix <- which.min(loss)
 	}
 
 	out <- list(loss = loss,
 				best_loss_ix = ix ,
-				mode = object$cluster_assignment[ix,] )
+				mode = object$cmat[ix,] )
 }
 
 
